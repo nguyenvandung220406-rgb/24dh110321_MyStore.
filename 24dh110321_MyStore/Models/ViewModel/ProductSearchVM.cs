d6cd5d90fb2529c92031544ac9a009d5dca89838
Nguyễn Văn Dũng <24DH110321@st.huflit.edu.vn>
2025-10-26 23:12:30 +0700

    cap nhat

---
+++ 24dh110321_MyStore/Models/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _24dh110321_MyStore.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        public string SearchTerm { get; set; }
+        
+        public decimal ? MinPrice { get; set; }
+        public decimal ? MaxPrice { get; set; }
+
+        public List<Product>Products { get; set; }
+    }
+}
